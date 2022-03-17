364cf4770c4702e1a3a1b69fc74a19d05be68f62
ekuinox <depkey@me.com>
2022-03-17 14:29:41 +0900

    とりあえず

---
+++ README.md
@@ -0,0 +1,11 @@
+# make-md-link
+
+見ているページを Markdown 形式の リンクとして生成するブックマークレット
+
+## ビルド
+
+`npm run build` でビルドできる
+
+`dist/bookmarklet.js` に出力される
+
+`javascrript: ...` 形式にするのを組み込んでいないのでそこは各々やってください
--- src/bookmarklet.ts
+++ src/bookmarklet.ts
@@ -1,1 +1,23 @@
-console.log('hello world');
+// markdown の リンクテキストを作成
+const createMarkdownLinkText = (title: string, url: string): string => {
+    return `[${title}](${url})`;
+};
+
+// 一番単純に作成する
+const createMarkdownLinkTextDefault = () => {
+    return createMarkdownLinkText(document.title, document.URL);
+};
+
+// 実行する
+const execute = (md: string) => {
+    // Clipboard API が有効であれば、クリップボードにコピーする
+    if (navigator.clipboard) {
+        navigator.clipboard.writeText(md);
+        return;
+    }
+
+    // Clipboard API が無効の場合はプロンプトを出してコピーできるようにしてやる
+    window.prompt('Markdown below', md);
+};
+
+execute(createMarkdownLinkTextDefault());
